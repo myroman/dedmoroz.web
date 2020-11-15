70f5754c85f67677078d4a65fd3966d9fcfecfc8
Roman Pavlushchenko <roman.pavlushchenko@gmail.com>
2020-11-14 17:24:14 -0800

    Add existing js + deploy scripts

---
+++ deploy_prod.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cp js/dm.settings.prod.js js/dm.settings.js
+echo 'Syncing to S3...'
+aws s3 sync . s3://darimchudo.ru --exclude "sync_s3.sh" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "*/.DS_Store" --exclude "scss/*" 
+echo 'Done'
